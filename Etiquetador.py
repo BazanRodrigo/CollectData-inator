3b0ee94a77a0f6beeb4d8f0a12eaa350a8ad966b
Rodrigo Bazán Zurita <bazanzuritarodrigo@gmail.com>
2020-12-30 15:53:06 -0600

    solo queda unir todo Bien hecho equipo.

--- Etiquetador.py
+++ Etiquetador.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import *
 import os
 import cv2
 import numpy as np
@@ -20,9 +21,6 @@ def medidas(xd, yd, xu, yu):
     largob = round(largob, 5)
     imagena = round(imagena, 5)
     imagenl = round(imagenl, 5)
-    print('CentroBounding box: ' , anchob, largob)
-    print('medidas de bounding box' , imagena, imagenl)
-    print('____________________________________')
     data = 0, anchob, largob, imagena, imagenl
     return str(data)
 
@@ -32,21 +30,20 @@ def savingData(data, lista, i):
     file.close()
 
 def main()  :
+    r = 0
     wp = pygame.image.load('/home/rodrigo/RecolectDataNator/DataProgram/WallpaperEtiqueta.jpg')
-    img = pygame.image.load('/home/rodrigo/RecolectDataNator/DataBase/v5.jpg')
+    img = pygame.image.load('/home/rodrigo/RecolectDataNator/DataBase/'+str(loader()[r])+'.jpg')
     save = pygame.image.load('/home/rodrigo/RecolectDataNator/Icons/save.png')
     next = pygame.image.load('/home/rodrigo/RecolectDataNator/Icons/next.png')
     pygame.init()
     pygame.display.init()
     window = pygame.display.set_mode()
     pygame.display.set_caption("Tag-inator")
-    font = pygame.font.SysFont("monospace", 30)
+    font = pygame.font.SysFont("arial", 30)
     window.blit(wp, (0,0))
     window.blit(img, (100,100))
-    pygame.draw.rect(window, (144, 12, 63), [492, 0, 720, 40])
     pygame.display.flip()
     running = True
-    r = 0
     while running:
         pygame.display.flip()
         # Did the user click the window close button?
@@ -62,17 +59,22 @@ def main()  :
                 xu = mu[0]
                 yu = mu[1]
                 if xu>768 and xu<800 and yu>50 and yu<82:
-                    print('Segun guardado XD')
                     savingData(data, loader(), r)
-                    img = pygame.image.load('/home/rodrigo/RecolectDataNator/DataBase/'+str(loader()[r])+'.jpg')
+                    r = r + 1
+                    try:
+                        img = pygame.image.load('/home/rodrigo/RecolectDataNator/DataBase/'+str(loader()[r])+'.jpg')
+                    except FileNotFoundError:
+                        finmessage = font.render('Proceso terminado', 2,(255, 160, 122), (88, 24, 69))
+                        window.blit(finmessage,(512,512))
+                        pygame.display.flip()
+                        pygame.time.wait(3000)
+                        running = False
                     window.blit(wp, (0,0))
                     window.blit(img, (100,100))
-                    r = r + 1
-                    print(r)
+                    pygame.display.flip()
                     continue
                 data = medidas(xd, yd, xu, yu)
-                pygame.draw.rect(window, (144, 12, 63), [492, 0, 720, 40])
-                message = font.render(data, 1,(255, 195, 0))
+                message = font.render(data, 1,(255, 195, 0),(144, 12, 63))
                 window.blit(message, (512,0))
                 window.blit(save, (768,50))
                 window.blit(next, (820,50))
@@ -86,14 +88,13 @@ def loader():
         try:
             point = elements.index('.')
         except ValueError:
-            print('')
+            continue
         terminacion = elements[point:]
         if terminacion != '.jpg' :
             lista.pop(j)
         lista[j] = (lista[j])[:point]
         j = j + 1
     lista = lista
-    print(len(lista))
     return lista
 
 if __name__ == '__main__':
